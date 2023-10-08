47125dc9cca000c1305e09f7a9b6c7546534fad9
dientd <duydien17031999@gmail.com>
2023-10-08 10:24:16 +0700

    feat: base user information module

---
+++ custom_addon/information/models/user_information.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError, ValidationError
+
+
+class UserInformation(models.Model):
+    _name = 'user.information'
+    _description = 'User Information'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = "full_name"
+
+    user_id = fields.Many2one(
+        'res.users',
+        string='User',
+        default=lambda self: self.env.context.get('user_id', self.env.user.id),
+        index=True,
+    )
+    full_name = fields.Char(string='Full Name', required=True, tracking=True)
+    date_of_birth = fields.Date(string="Date of Birth", required=True)
+    sex = fields.Selection([('m', 'Male'), ('f', 'Female')], string="Sex", required=True)
+    image = fields.Binary(string="Image")
+    marital_status = fields.Selection(
+        [('s', 'Single'), ('m', 'Married'), ('w', 'Widowed'), ('d', 'Divorced'), ('x', 'Seperated')],
+        string='Marital Status')
+    department = fields.Char(string='Department')
+    id_number = fields.Char(string='ID Number')
+    institution = fields.Char(string='Institution')
+    address = fields.Char(string='Add')
+    account_number = fields.Char(string='Account Number')
+    openned_at = fields.Char(string='Oppenned At')
+    bank = fields.Char(string='Bank Brank')
+
+    def user_information_action(self):
+        user_id = self.env['user.information'].search([('user_id', '=', self._uid)])
+        action = {
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "res_model": "user.information",
+            "res_id": user_id.id,
+        }
+        return action
